d535936d57522e1991840e853396634f0c8f8d8b
john0248 <43078788+john0248@users.noreply.github.com>
2021-03-17 19:39:03 -0700

    Add files via upload

---
+++ Queries/queries.sql
@@ -0,0 +1,22 @@
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Creating new tables for Retirement eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--allows me to see the table
+SELECT * FROM retirement_info;
+
